55ad519e5ee5e173f7f6a15bf27cf6a2e2ef9dd5
kasimnetarioinnovations <kasimnetarioinnovations@gmail.com>
2024-12-20 01:09:09 -0800

    footer

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../Components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
